b2a122e34f8d6a00c22d2ecc4e192f4821df3b38
mickoo <92micko@live.com>
2014-12-16 15:06:56 +0100

    OS2-projekat:prvi dan(mount zavrseno)

--- OS2-projekat/KernelFS.cpp
+++ OS2-projekat/KernelFS.cpp
@@ -1,4 +1,27 @@
 #include "KernelFS.h"
 #include "part.h"
+#include <iostream>
+#include <cstdlib>
+
 
 char KernelFS::slova[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+Partition* KernelFS::particije[]={ NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL };
+int KernelFS::pop = 0;
+
+char KernelFS::mount(Partition* p)
+{
+	if (pop == 26)return '0';
+	else
+	{
+		for (int i = 0; i < 26; i++)
+		{
+			if (particije[i] == NULL){
+				particije[i] = p;
+				pop++;
+				return slova[i];
+			}
+		}
+	}
+
+}
+
--- OS2-projekat/KernelFS.h
+++ OS2-projekat/KernelFS.h
@@ -1,10 +1,18 @@
 #ifndef _Kernel_FS_
 #define _Kernel_FS_
 
+#include "part.h"
+
 class KernelFS{
+	
 public:
 
 	static char slova[26];
+	static Partition *particije[26];
+	static int pop;
+
+
+	static char mount(Partition* p);
 
 };
 
